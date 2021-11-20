93582392010bb4e3c9040555bd4ebdd438303579
Kannancin <94105858+Kannancin@users.noreply.github.com>
2021-11-20 12:11:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -193,3 +193,9 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
+else:
+        await bot.send_photo(
+        chat_id=message.from_user.id,
+        photo="https://telegra.ph/file/75f17e0c5ec8efa06263b.jpg",
+        caption=START_MSG.format(message.from_user.first_name),
+        parse_mode="html",
